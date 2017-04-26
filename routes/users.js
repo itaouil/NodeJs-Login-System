8ccc3b524ce97d4f3d97e2bdd083db488e91890b
Ilyass Taouil <itaouil95@gmail.com>
2017-04-26 08:11:29 +0200

    Added register and login views. Houston.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  next();
+});
+
+// Register
+router.get('/register', function(req, res, next) {
+  res.render('register', {flag: "Register"})
+});
+
+// Register
+router.get('/login', function(req, res, next) {
+  res.render('login', {flag: 'Login'})
 });
 
 module.exports = router;
