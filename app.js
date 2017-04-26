0ae7c9402b3d216bbd15d8297edad59f7f43ba11
Ilyass Taouil <itaouil95@gmail.com>
2017-04-26 07:10:53 +0200

    Set up middlewares, houston. Proceeding with views.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger        = require('morgan');
 var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 var session       = require('express-session');
+var validator     = require('express-validator')
 var passport      = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var multer        = require('multer');
@@ -15,6 +16,9 @@ var mongoose      = require('mongoose');
 
 // Database connection instance
 var db = mongoose.connection;
+
+// Multer middleware
+var uploads = multer({dest: "./uploads"});
 
 // Routes modules
 var index = require('./routes/index');
@@ -35,6 +39,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  saveUninitialized: true,
+  resave: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Expess validator middleware
+app.use(validator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Express Messages validator
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 // Routes middlewares usage
 app.use('/', index);
